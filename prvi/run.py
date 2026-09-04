2ad8e72c19cc69c042516165474f240c7703b30e
mbaloh988 <59625993+mbaloh988@users.noreply.github.com>
2026-09-04 06:51:30 +0000

    ustvarili prvi flask

--- prvi/run.py
+++ prvi/run.py
@@ -1,1 +1,12 @@
-print("Hello 2.Ri ♥")
+from flask import Flask
+app = Flask(__name__)
+
+APP_ADDRESS = "0.0.0.0"
+APP_PORT = 5000
+
+app.config["DEBUG"] = True
+app.run(host = APP_ADDRESS, port = APP_PORT)
+
+@app.route("/")
+def hello_world():
+    return "Hello 2.Ri! ♥"
