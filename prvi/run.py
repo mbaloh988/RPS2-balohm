133f002614372bcb86506c3f1f548e0a7b009434
mbaloh988 <59625993+mbaloh988@users.noreply.github.com>
2026-09-04 11:43:58 +0000

    App run mora biti zadnja vrstica

--- prvi/run.py
+++ prvi/run.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 APP_ADDRESS = "0.0.0.0"
 APP_PORT = 5000
 
+
+
+@app.route("/", methods = ["GET", "POST"])
+def hello_world():
+    return "Hello 2.Ri! ♥"
+
 app.config["DEBUG"] = True
 app.run(host = APP_ADDRESS, port = APP_PORT)
 
-@app.route("/")
-def hello_world():
-    return "Hello 2.Ri! ♥"
