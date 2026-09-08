e7545fb26c9bc1dfc9220b00396dc20d5269ab3f
mbaloh988 <59625993+mbaloh988@users.noreply.github.com>
2026-09-08 10:12:18 +0000

    Uporabljati smo začeli render_tempalte

--- prvi/run.py
+++ prvi/run.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 APP_ADDRESS = "0.0.0.0"
@@ -8,7 +10,8 @@ APP_PORT = 5000
 
 @app.route("/", methods = ["GET", "POST"])
 def hello_world():
-    return "Hello 2.Ri! ♥"
+
+
 
 app.config["DEBUG"] = True
 app.run(host = APP_ADDRESS, port = APP_PORT)
