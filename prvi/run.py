103ec4e031bf31952a0e8fad427c41baa065fd3a
Miha <miha.baloh@sckr.si>
2026-09-11 06:51:43 +0200

    Pripravil sem dependencies/requirements.txt

--- prvi/run.py
+++ prvi/run.py
@@ -4,15 +4,16 @@ from flask import render_template
 app = Flask(__name__)
 
 APP_ADDRESS = "0.0.0.0"
-APP_PORT = 5000
+APP_PORT = 80
 
-
+# GLAVNI ROUTE APLIKACIJE
 
 @app.route("/", methods = ["GET", "POST"])
 def hello_world():
+    return render_template("index.html")
 
 
-
+# ZAGON APLIKACIJE
 app.config["DEBUG"] = True
 app.run(host = APP_ADDRESS, port = APP_PORT)
 
